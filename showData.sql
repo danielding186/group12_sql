abd1dd2475193e4a66141aa7594661ab9b955379
glintstar <glintstar@gmail.com>
2020-04-10 17:19:28 -0700

    update location for media

--- showData.sql
+++ showData.sql
@@ -36,4 +36,23 @@ select dbo.CheckBlockInfo(1) as blocker;
 
 select top 10 * from MediaCount order by like_counts desc;
 
+
+-- SET NOCOUNT On
+-- Declare @Start int = (select min(media_id) from Media);
+-- Declare @End int = (select max(media_id) from Media);
+-- while @Start <= @End 
+-- BEGIN
+--     if (rand() < 0.7)
+--         update Media set location_id = 471 + cast(rand() * 26 as int) where media_id = @Start;
+--     else
+--         update Media set location_id = 494 where media_id = @Start;
+
+--     Set @Start += 1;
+-- End
+
+select location_id, count(*) as [count] from Media group by location_id order by location_id;
+
+select * from [Location];
+
+
 go
